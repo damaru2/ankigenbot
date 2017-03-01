02e9ae58746363c44c3ab827642d435d312b2c44
damaru <david_martirubio@hotmail.com>
2017-03-01 16:10:45 +0000

    change send_card.py after interface change of ankiweb

--- src/send_card.py
+++ src/send_card.py
@@ -20,6 +20,7 @@ class CardSender:
         pass_box.send_keys(password + '\n')
 
     def send_card(self, front, back, deck):
+        '''
         # Click on add
         self.driver.find_element_by_xpath(
             '//*[@id="headerTable"]/tbody/tr[2]/td/a[3]').click()
@@ -39,6 +40,27 @@ class CardSender:
             pass
         self.driver.find_element_by_xpath(
             '//*[@id="modelarea"]/tbody/tr[1]/td[3]/button').click()
+        self.driver.quit()
+        '''
+        self.driver.find_element_by_xpath(
+            '//*[@id="navbarSupportedContent"]/ul[1]/li[2]/a').click()
+        try:
+            deck_box = self.driver.find_element_by_id('deck')
+            deck_box.clear()
+            deck_box.send_keys(deck)
+        except:
+            pass
+        try:
+            self.driver.find_element_by_xpath('//*[@id="f0"]').send_keys(front)
+        except:
+            pass
+        try:
+            self.driver.find_element_by_id('f1').send_keys(back)
+        except:
+            pass
+        self.driver.find_element_by_xpath(
+            '/html/body/main/p/button').click()
+
         self.driver.quit()
 
 if __name__ == "__main__":
